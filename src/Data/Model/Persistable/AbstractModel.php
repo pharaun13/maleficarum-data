7543bf96dc5cc207b046bebbc0a871668f756851
Adam Kowalski <adam.kowalski@miinto.com>
2018-06-19 15:39:58 +0200

    Change persistable model namespace

--- src/Data/Model/Persistable/AbstractModel.php
+++ src/Data/Model/Persistable/AbstractModel.php
@@ -4,36 +4,36 @@
  */
 declare(strict_types=1);
 
-namespace Maleficarum\Data\Model;
+namespace Maleficarum\Data\Model\Persistable;
 
-abstract class AbstractPersistableModel extends AbstractModel {
+abstract class AbstractModel extends \Maleficarum\Data\Model\AbstractModel {
     /**
      * Persist data stored in this model as a new storage entry.
      *
-     * @return \Maleficarum\Data\Model\AbstractPersistableModel|$this enables method chaining
+     * @return \Maleficarum\Data\Model\Persistable\AbstractModel|$this enables method chaining
      */
-    abstract public function create(): \Maleficarum\Data\Model\AbstractPersistableModel;
+    abstract public function create(): \Maleficarum\Data\Model\Persistable\AbstractModel;
 
     /**
      * Refresh this model with current data from the storage
      *
-     * @return \Maleficarum\Data\Model\AbstractPersistableModel|$this enables method chaining
+     * @return \Maleficarum\Data\Model\Persistable\AbstractModel|$this enables method chaining
      */
-    abstract public function read(): \Maleficarum\Data\Model\AbstractPersistableModel;
+    abstract public function read(): \Maleficarum\Data\Model\Persistable\AbstractModel;
 
     /**
      * Update storage entry with data currently stored in this model.
      *
-     * @return \Maleficarum\Data\Model\AbstractPersistableModel|$this enables method chaining
+     * @return \Maleficarum\Data\Model\Persistable\AbstractModel|$this enables method chaining
      */
-    abstract public function update(): \Maleficarum\Data\Model\AbstractPersistableModel;
+    abstract public function update(): \Maleficarum\Data\Model\Persistable\AbstractModel;
 
     /**
      * Delete an entry from the storage based on ID data stored in this model
      *
-     * @return \Maleficarum\Data\Model\AbstractPersistableModel|$this enables method chaining
+     * @return \Maleficarum\Data\Model\Persistable\AbstractModel|$this enables method chaining
      */
-    abstract public function delete(): \Maleficarum\Data\Model\AbstractPersistableModel;
+    abstract public function delete(): \Maleficarum\Data\Model\Persistable\AbstractModel;
 
     /**
      * Validate data stored in this model to check if it can be persisted in storage.
